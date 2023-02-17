8a804585a4674f56a263c916a27551d7484bf3b3
= <=>
2023-02-17 23:37:26 +0700

    crud vs react 2 (show data users)

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,69 @@
 import React, { Component } from 'react';
+import { Fragment } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+
+import userService from '../../services/userService';
+
+import './UserManage.scss';
+
 class UserManage extends Component {
 
-    state = {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            arrUsers: []
+        }
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await userService.getAllUsers('ALL');
+        if(response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users,
+            })
+        }
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className='users-container'>
+                <div className="title text-center">Tien Basic</div>
+                <div className='users-table mt-3 mx-1'>
+                <table id="customers">
+                    <tbody>
+                        <tr>
+                            <th>Email</th>
+                            <th>FirstName</th>
+                            <th>LastName</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {
+                            arrUsers && arrUsers.map((arrUser, index) => {
+                                return (
+                                    <Fragment>
+                                        <tr>
+                                            <td>{arrUser.email}</td>
+                                            <td>{arrUser.firstName}</td>
+                                            <td>{arrUser.lastName}</td>
+                                            <td>{arrUser.address}</td>
+                                            <td>
+                                                <button className='btn-edit'><i className="fas fa-edit"></i></button>
+                                                <button className='btn-delete'><i className="fas fa-trash-alt"></i></button>
+                                            </td>
+                                        </tr>
+                                    </Fragment>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -5,6 +5,10 @@ const userService = {
         return axios.post('/api/login', {email, password});
     },
 
+    getAllUsers(id) {
+        return axios.get(`/api/get-all-users?id=${id}`)
+    }
+
 }
 
 export default userService;
